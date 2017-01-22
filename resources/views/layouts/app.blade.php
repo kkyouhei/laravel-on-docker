7d051761be8ad0435507d9689aaa9e6c6aaa1669
kkyouhei <hope637@gmail.com>
2017-01-22 22:04:07 +0900

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+// resources/views/layouts/app.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Laravel Quickstart - Basic</title>
+  </head>
+
+  <body>
+    <div class="container">
+    <nav class="navbar navbar-default">
+      <!-- ナビバーの内容 -->
+      </nav>
+    </div>
+
+    @yield('content')
+  </body>
+</html>
